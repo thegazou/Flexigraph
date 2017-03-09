dc0572343ee86784c12cec14b52eb24736689cf2
vinch88 <vincent.chaperon@gmail.com>
2017-03-09 14:34:01 +0100

    Example show, resize and display

--- src/Example.java
+++ src/Example.java
@@ -1,13 +1,47 @@
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
-public class Example
-{
-   public static void main( String[] args )
-   {
-      System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
-      Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
-      System.out.println( "mat = " + mat.dump() );
-   }
+public class Example {
+
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+		System.out.println("mat = " + mat.dump());
+		Mat image = Imgcodecs.imread("pictures/DSC_0045.JPG");
+		showResult(image);
+
+	}
+
+	public static void showResult(Mat img) {
+		Imgproc.resize(img, img, new Size(640, 480));
+		MatOfByte matOfByte = new MatOfByte();
+		Imgcodecs.imencode(".jpg", img, matOfByte);
+		byte[] byteArray = matOfByte.toArray();
+		BufferedImage bufImage = null;
+		try {
+			InputStream in = new ByteArrayInputStream(byteArray);
+			bufImage = ImageIO.read(in);
+			JFrame frame = new JFrame();
+			frame.getContentPane().add(new JLabel(new ImageIcon(bufImage)));
+			frame.pack();
+			frame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
