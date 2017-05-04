77ba67837e20e3cb9eb720034b949aacd4416949
loris <ceschinlori@gmail.com>
2017-05-04 09:05:44 +0200

    UI + processing

--- src/processing/Main.java
+++ src/processing/Main.java
@@ -1,9 +1,18 @@
 package processing;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import pre_processing.Perspective;
 import tools.Tools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 
 public class Main {
 
@@ -15,11 +24,39 @@ public class Main {
 
 
         Processing p = new Processing();
-        Mat fft = p.fourierTransform("pictures/image_scan.png");
-        Mat image = p.inverseFourierTransform();
+        List<Mat> sources = new ArrayList<>();
+        Mat original = Imgcodecs.imread("pictures/image_scan.png", Imgcodecs.CV_LOAD_IMAGE_COLOR);
+
+        //original = Perspective.correctPerspective();
+
+        Core.split(original, sources);
+
+        List<Mat> results = new ArrayList<>();
+        for (Mat m : sources) {
 
 
-        Imgproc.threshold(image, image, 200, 255, Imgproc.THRESH_BINARY);
+            m.convertTo(m, CvType.CV_32FC1);
+            Mat fft = p.fourierTransform(m);
+
+            Mat img = p.inverseFourierTransform();
+            results.add(img);
+        }
+
+        Mat image = new Mat();
+
+        for (Mat m : results)
+        {
+            Imgproc.threshold(m, m, 200, 255, Imgproc.THRESH_BINARY);
+            Tools.showResult(m);
+        }
+
+        Core.merge(results, image);
+
+
+
         Tools.showResult(image);
+        Imgproc.threshold(image, image, 190, 255, Imgproc.THRESH_BINARY);
+        //Imgproc.threshold(image, image, 115, 255, Imgproc.THRESH_BINARY);
+        //Tools.showResult(image);
     }
 }
--- src/tools/Tools.java
+++ src/tools/Tools.java
@@ -1,6 +1,7 @@
 package tools;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
@@ -9,6 +10,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.core.Size;
@@ -18,7 +20,7 @@ import org.opencv.imgproc.Imgproc;
 public class Tools {
 
 	public static void showResult(Mat img) {
-		Imgproc.resize(img, img, new Size(img.width()/2, img.height() / 2));
+		//Imgproc.resize(img, img, new Size(img.width()/2, img.height() / 2));
 		MatOfByte matOfByte = new MatOfByte();
 		Imgcodecs.imencode(".jpg", img, matOfByte);
 		byte[] byteArray = matOfByte.toArray();
@@ -33,5 +35,32 @@ public class Tools {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static BufferedImage matToBufferedImage(Mat img) {
+		MatOfByte matOfByte = new MatOfByte();
+		Imgcodecs.imencode(".jpg", img, matOfByte);
+		byte[] byteArray = matOfByte.toArray();
+		BufferedImage bufImage = null;
+		try {
+			InputStream in = new ByteArrayInputStream(byteArray);
+			bufImage = ImageIO.read(in);
+			return bufImage;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+			return null;
+	}
+
+	public static Mat bufferedImageToMat(BufferedImage image) {
+		// REMOVE ALPHA LAYER
+		BufferedImage convertedImg = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+		convertedImg.getGraphics().drawImage(image, 0, 0, null);
+		convertedImg.getGraphics().dispose();
+
+		byte[] data = ((DataBufferByte) convertedImg.getRaster().getDataBuffer()).getData();
+		Mat mat = new Mat(convertedImg.getHeight(), convertedImg.getWidth(), CvType.CV_8UC3);
+		mat.put(0, 0, data);
+		return mat;
 	}
 }
